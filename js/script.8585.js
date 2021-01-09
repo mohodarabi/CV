e57a0c610aef15413ba3b8d2fd8f84c385c35ba3
Hosein Darabi <56962999+mho3eind@users.noreply.github.com>
2021-01-09 10:45:21 +0330

    Add files via upload

---
+++ js/script.8585.js
@@ -0,0 +1,9 @@
+let right = document.querySelector('.right')
+let left = document.querySelector('.left')
+
+if (window.innerWidth <= 750) {
+
+    right.classList.remove('hvr-grow-shadow')
+    left.classList.remove('hvr-bob')
+
+} 
